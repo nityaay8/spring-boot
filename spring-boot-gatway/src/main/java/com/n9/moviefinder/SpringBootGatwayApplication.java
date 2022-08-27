ca828213c237dee433118cde65cdb3c91c445dff
nityaay8 <nityaay8@gmail.com>
2022-08-27 17:54:03 +0530

    gateway initial code

---
+++ spring-boot-gatway/src/main/java/com/n9/moviefinder/SpringBootGatwayApplication.java
@@ -0,0 +1,13 @@
+package com.n9.moviefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGatwayApplication.class, args);
+	}
+
+}
---
+++ spring-boot-gatway/src/test/java/com/n9/moviefinder/SpringBootGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n9.moviefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
